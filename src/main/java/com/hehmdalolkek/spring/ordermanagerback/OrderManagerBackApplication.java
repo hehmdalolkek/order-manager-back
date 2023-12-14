7f4d565b2f02e0aa98b9d3eb08a198fdc6f0746a
hehmdalolkek <badeha.inna@yandex.ru>
2023-12-14 16:05:32 +0400

    init commit

---
+++ src/main/java/com/hehmdalolkek/spring/ordermanagerback/OrderManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.ordermanagerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehmdalolkek/spring/ordermanagerback/OrderManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.ordermanagerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
